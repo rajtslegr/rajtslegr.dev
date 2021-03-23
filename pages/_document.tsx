2f52f11f9199546af356720acaf9553158fdf79f
Petr Rajtslegr <petr.rajtslegr@gmail.com>
2021-03-23 15:54:50 +0100

    head, gtag

---
+++ pages/_document.tsx
@@ -0,0 +1,38 @@
+import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    const GA_TRACKING_ID = process.env.GTAG;
+    return (
+      <Html>
+        <Head>
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+          <script
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{
+              __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${GA_TRACKING_ID}', {
+                    page_path: window.location.pathname,
+                    });
+                `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import { NextPage } from 'next';
+import Head from 'next/head';
 import React from 'react';
 
 const IndexPage: NextPage = () => {
   return (
     <>
+      <Head>
+        <title>Petr Rajtslegr</title>
+      </Head>
       <div className="container flex justify-center mx-auto">
         <div className="flex justify-center items-center w-full m-4 p-4">
           <h1 className="mb-4 font-semibold">Coming soon...</h1>

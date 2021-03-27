764cb47b1b9d532997db8d5a8a1ec4ff97968d07
Petr Rajtslegr <petr.rajtslegr@gmail.com>
2021-03-27 12:56:32 +0100

    layout cont

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -10,7 +10,9 @@ const Layout: React.FC<Props> = ({ children }) => {
   return (
     <div className="flex flex-col items-center min-h-screen">
       <NavBar />
-      <main className="flex-auto w-full pt-16">{children}</main>
+      <main className="flex-auto w-full pt-16">
+        <div className="container lg:w-4/6 mx-auto m-4 p-4">{children}</div>
+      </main>
       <Footer />
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,10 +8,7 @@ const IndexPage: NextPage = () => {
       <Head>
         <title>Petr Rajtslegr | Full-stack Dev</title>
       </Head>
-      <div className="container flex flex-col items-center mx-auto m-4 p-4">
-        <h1 className="mb-4 font-semibold">Coming soon...</h1>
-        <img className="w-1/2 sm:w-1/3" src="/maintance.svg" alt="Maintance Art" />
-      </div>
+      <div>Home works!</div>
     </>
   );
 };

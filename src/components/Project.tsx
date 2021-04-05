aab045007a917c5f59bc3ee3977b77076ea0d68c
Petr Rajtslegr <petr.rajtslegr@gmail.com>
2021-04-05 22:50:05 +0200

    skeleton

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from 'next';
+import React, { useEffect, useRef, useState } from 'react';
 import { IProject } from '../types/types';
 
 interface Props {
@@ -6,14 +7,24 @@ interface Props {
 }
 
 const Project: NextPage<Props> = ({ project: { title, description, build, image, repo } }) => {
+  const [loaded, setLoaded] = useState(false);
+  const ref = useRef<HTMLImageElement>(null);
+
+  useEffect(() => {
+    if (ref?.current?.complete) setLoaded(true);
+  }, []);
+
   return (
     <div className="flex flex-col overflow-hidden transition border rounded shadow hover:shadow-lg">
       <a href={repo} rel="noopener noreferrer" target="_blank">
-        <img
-          className="w-full border-b"
-          src={`/static/images/projects/${image}`}
-          alt={`${title} mockup`}
-        />
+        <div className={`${!loaded ? 'bg-gray-300 animate-pulse' : null}`}>
+          <img
+            ref={ref}
+            className={`w-full border-b  ${loaded ? 'opacity-100' : 'opacity-0'}`}
+            src={`/static/images/projects/${image}`}
+            alt={`${title} mockup`}
+          />
+        </div>
         <div className="flex flex-col px-2 py-4 sm:px-4">
           <div className="mb-2 text-xl font-bold">{title}</div>
           <p className="text-base text-gray-500 dark:text-gray-400">{description}</p>

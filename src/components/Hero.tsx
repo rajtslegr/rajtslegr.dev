a61f52cb2666e9ade7e8c5d341c6cc6b2260f383
Petr Rajtslegr <petr.rajtslegr@gmail.com>
2021-04-06 11:14:17 +0200

    hero shadow fix

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,14 +4,13 @@ const Hero: React.FC = () => {
   return (
     <div className="flex flex-col items-center p-2 lg:flex-row lg:justify-around">
       <Image
+        className="rounded-full shadow"
         src="/static/images/hero.png"
         alt="Hero"
-        className="rounded-full shadow-xl"
-        width={334}
+        width={344}
         height={344}
         priority
       ></Image>
-
       <div className="hidden m-6 xl:block">
         <p className="mb-4 text-4xl">Petr Rajtslegr</p>
         <p className="text-2xl text-gray-500 dark:text-gray-400">Hello!</p>

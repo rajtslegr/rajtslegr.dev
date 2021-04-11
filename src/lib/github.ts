9dc0d9c63f2c861191307b3402ad23fdd8dcd147
Petr Rajtslegr <petr.rajtslegr@gmail.com>
2021-04-12 01:00:21 +0200

    api fixes

--- src/lib/github.ts
+++ src/lib/github.ts
@@ -1,10 +1,10 @@
-export const getRecentRepos = async (): Promise<Response | null> => {
+export const getRecentRepos = async (): Promise<Response | string> => {
   try {
     const res = await fetch(
       'https://api.github.com/users/rajcep/repos?per_page=6&sort=pushed&direction=desc',
     );
     return await res.json();
   } catch (e) {
-    return null;
+    return e.message;
   }
 };
--- src/lib/instagram.ts
+++ src/lib/instagram.ts
@@ -1,10 +1,10 @@
-export const getRecentPosts = async (): Promise<Response | null> => {
+export const getRecentPosts = async (): Promise<Response | string> => {
   try {
     const res = await fetch(
       `https://graph.instagram.com/me/media?fields=id,permalink,media_url,thumbnail_url,caption&access_token=${process.env.INSTAGRAM_TOKEN}`,
     );
     return await res.json();
   } catch (e) {
-    return null;
+    return e.message;
   }
 };

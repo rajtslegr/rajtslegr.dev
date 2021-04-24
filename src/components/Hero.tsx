ccead5dfc3f2cdff2594d5e4f54d1e31698c19e4
Petr Rajtslegr <petr.rajtslegr@gmail.com>
2021-04-24 18:38:53 +0200

    hero spacing

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,15 @@ import Image from 'next/image';
 
 const Hero: React.FC = () => {
   return (
-    <div className="flex flex-col items-center p-2 lg:flex-row lg:justify-around">
+    <div className="flex flex-col-reverse items-center p-2 gap-y-8 lg:flex-row lg:justify-around">
+      <div className="block lg:max-w-md">
+        <p className="mb-4 text-4xl">Petr Rajtslegr</p>
+        <p className="text-2xl text-gray-500 dark:text-gray-400">Hello!</p>
+        <p className="text-2xl text-gray-500 dark:text-gray-400">
+          I am developer based in Prague, Czech Republic and I love to create
+          things!
+        </p>
+      </div>
       <Image
         className="rounded-full shadow"
         src="/static/images/hero.png"
@@ -11,14 +19,6 @@ const Hero: React.FC = () => {
         height={344}
         priority
       ></Image>
-      <div className="hidden m-6 xl:block">
-        <p className="mb-4 text-4xl">Petr Rajtslegr</p>
-        <p className="text-2xl text-gray-500 dark:text-gray-400">Hello!</p>
-        <p className="text-2xl text-gray-500 dark:text-gray-400">
-          I am developer based in Prague, Czech Republic and I love to create
-          things!
-        </p>
-      </div>
     </div>
   );
 };

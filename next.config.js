2075118bda767f7f3263ed00dc66bfa256ffcaed
Petr Rajtslegr <petr.rajtslegr@gmail.com>
2021-06-12 12:33:57 +0200

    Preact - client production build

--- next.config.js
+++ next.config.js
@@ -3,6 +3,18 @@ module.exports = {
   images: {
     domains: ['lastfm.freetls.fastly.net'],
   },
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      });
+    }
+
+    return config;
+  },
   async headers() {
     return [
       {

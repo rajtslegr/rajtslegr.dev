5ea97edffd45f550e562cda10bafd430ea0afc24
Petr Rajtslegr <petr.rajtslegr@gmail.com>
2021-06-17 15:54:55 +0200

    next/script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,37 @@
 import Layout from 'components/Layout';
 import { ThemeProvider } from 'next-themes';
 import { AppProps } from 'next/app';
+import Script from 'next/script';
 import 'styles/index.css';
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
+  const GA_TRACKING_ID = process.env.GTAG;
+
   return (
-    <ThemeProvider defaultTheme="light" attribute="class">
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeProvider>
+    <>
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+      <Script
+        // eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+            page_path: window.location.pathname,
+            });
+        `,
+        }}
+      />
+      <ThemeProvider defaultTheme="light" attribute="class">
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </>
   );
 };
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -21,28 +21,9 @@ class MyDocument extends Document {
   }
 
   render(): JSX.Element {
-    const GA_TRACKING_ID = process.env.GTAG;
-
     return (
       <Html lang="en">
         <Head>
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-          />
-          <script
-            // eslint-disable-next-line react/no-danger
-            dangerouslySetInnerHTML={{
-              __html: `
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', '${GA_TRACKING_ID}', {
-                    page_path: window.location.pathname,
-                    });
-                `,
-            }}
-          />
           <link rel="preconnect" href="https://www.google-analytics.com" />
           <link
             rel="shortcut icon"

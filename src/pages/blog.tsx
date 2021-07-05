53d99407a886a1f34e6171f3e391cfeb1f862292
Petr Rajtslegr <petr.rajtslegr@gmail.com>
2021-07-05 15:23:00 +0200

    posts search

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,7 +1,10 @@
+import { SearchIcon } from '@heroicons/react/solid';
+import Input from 'components/Input';
 import MetaData from 'components/MetaData';
 import { getSortedPostsData } from 'lib/posts';
 import { GetStaticProps, NextPage } from 'next';
 import Link from 'next/link';
+import React, { useState } from 'react';
 import { ISortedPostData } from 'types/types';
 import parseDate from 'utils/date';
 
@@ -10,12 +13,27 @@ interface Props {
 }
 
 const Blog: NextPage<Props> = ({ allPostsData }) => {
+  const [search, setSearch] = useState('');
+  const filteredPosts = allPostsData.filter((post) =>
+    post.title.toLowerCase().includes(search.toLowerCase()),
+  );
+
   return (
     <>
       <MetaData title="Petr Rajtslegr | Blog" />
       <h1 className="mb-4 text-4xl font-bold md:mb-12">Blog</h1>
+      <div className="relative flex flex-col w-full mb-4 md:w-1/3">
+        <Input
+          label="Search"
+          placeholder="Search..."
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <span className="absolute w-5 h-5 text-gray-500 right-3 top-[38px] dark:text-gray-400">
+          <SearchIcon />
+        </span>
+      </div>
       <ul className="grid w-full gap-4 lg:grid-cols-2">
-        {allPostsData.map(({ id, date, title, description, readingTime }) => (
+        {filteredPosts.map(({ id, date, title, description, readingTime }) => (
           <li key={id}>
             <Link href={`/post/${id}`} passHref>
               <div className="flex flex-col p-4 space-y-2 transition bg-white rounded shadow cursor-pointer hover:shadow-lg dark:bg-card">
@@ -34,6 +52,11 @@ const Blog: NextPage<Props> = ({ allPostsData }) => {
           </li>
         ))}
       </ul>
+      {filteredPosts.length < 1 && (
+        <p className="flex justify-center p-6 italic text-gray-500 dark:text-gray-400">
+          These aren&apos;t the droids you&apos;re looking for...
+        </p>
+      )}
     </>
   );
 };

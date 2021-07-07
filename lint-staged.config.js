d3df8af9f6a1a0ae6146b9665c6861d694cfbbfc
Petr Rajtslegr <petr.rajtslegr@gmail.com>
2021-07-07 16:17:47 +0200

    fixed tests

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-  // Run tests on related files
-  '**/*.spec.ts?(x)': () => 'pnpm run test:related',
   // Run type-check on changes to TypeScript files
-  '**/*.ts?(x)': () => 'pnpm run type-check',
+  '**/*.ts?(x)': (filenames) => [
+    'pnpm run type-check',
+    `pnpm run test:related ${filenames.join(' ')}`,
+  ],
   // Run Prettier and ESLint on all JavaScript/TypeScript files
   '**/*.(ts|js|mdx)?(x)': (filenames) => [
     'pretty-quick --staged',

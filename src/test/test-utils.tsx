5cb6d804ed12b1572190fed7607c427fa9af2884
Petr Rajtslegr <petr.rajtslegr@gmail.com>
2021-08-11 19:36:43 +0200

    next.js 11.1

--- src/test/test-utils.tsx
+++ src/test/test-utils.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable */
 import { render as defaultRender } from '@testing-library/react';
-import { RouterContext } from 'next/dist/next-server/lib/router-context';
+import { RouterContext } from 'next/dist/shared/lib/router-context';
 import { NextRouter } from 'next/router';
 
 export * from '@testing-library/react';

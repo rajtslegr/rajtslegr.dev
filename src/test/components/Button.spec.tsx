44bdb26bd67b2c64844e7acdd55011e2b773d90b
Petr Rajtslegr <petr.rajtslegr@gmail.com>
2021-08-13 18:18:55 +0200

    button test

---
+++ src/test/components/Button.spec.tsx
@@ -0,0 +1,14 @@
+import Button from '@/components/ui/Button';
+import '@testing-library/jest-dom';
+import { screen } from '@testing-library/react';
+import { render } from '../test-utils';
+
+describe('button', () => {
+  it('should contain given children', () => {
+    const innerHTML = 'My name is button!';
+    render(<Button>{innerHTML}</Button>);
+
+    const text = screen.getByText(innerHTML);
+    expect(text).toBeInTheDocument();
+  });
+});

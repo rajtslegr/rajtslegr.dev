145933559cad97775c497b2c818af32aae627501
Petr Rajtslegr <petr.rajtslegr@gmail.com>
2021-10-27 13:29:16 +0200

    :arrow_up: Update dependencies

--- src/test/test-utils.tsx
+++ src/test/test-utils.tsx
@@ -24,6 +24,14 @@ export function render(
   return defaultRender(ui, { wrapper, ...options });
 }
 
+beforeAll(() => {
+  jest.spyOn(console, 'log').mockImplementation(() => {});
+  jest.spyOn(console, 'error').mockImplementation(() => {});
+  jest.spyOn(console, 'warn').mockImplementation(() => {});
+  jest.spyOn(console, 'info').mockImplementation(() => {});
+  jest.spyOn(console, 'debug').mockImplementation(() => {});
+});
+
 const mockRouter: any = {
   basePath: '',
   pathname: '/',

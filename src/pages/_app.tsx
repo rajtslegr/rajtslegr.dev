76f6bb828c809263b3ff67d80a34d4e80078ffd6
Petr Rajtslegr <petr.rajtslegr@gmail.com>
2021-12-13 23:12:10 +0100

    :lipstick: Add progress bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import '@/styles/globals.css';
 import { AnimatePresence } from 'framer-motion';
 import { ThemeProvider } from 'next-themes';
 import { AppProps } from 'next/app';
+import NextNProgress from 'nextjs-progressbar';
 
 const App: React.FC<AppProps> = ({ Component, pageProps, router }) => {
   usePanelbear('8nvtaxIaLJM');
@@ -16,6 +17,14 @@ const App: React.FC<AppProps> = ({ Component, pageProps, router }) => {
     >
       <AnimatePresence exitBeforeEnter>
         <Layout>
+          <NextNProgress
+          // color={
+          //   resolvedTheme === 'light'
+          //     ? 'rgb(229, 231, 235)'
+          //     : 'rgb(55, 65, 81)'
+          // }
+          // options={{ showSpinner: false }}
+          />
           <Component {...pageProps} key={router.route} />
         </Layout>
       </AnimatePresence>

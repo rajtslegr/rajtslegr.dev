695ee5f89b4d5fe9f784550df3b16810ecf56b8c
Petr Rajtslegr <petr.rajtslegr@gmail.com>
2022-04-02 13:02:07 +0200

    :arrow_up: Update dependencies

--- src/components/instagram/InstagramItem.tsx
+++ src/components/instagram/InstagramItem.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import clsx from 'clsx';
 import Image from 'next/image';
 
@@ -8,27 +10,34 @@ export interface Props {
   edge: InstagramDataEdge;
 }
 
-const InstagramItem: React.FC<Props> = ({ index, edge }) => (
-  <div
-    key={edge.id}
-    className={clsx(
-      index === 0 ? 'lg:col-span-2 lg:row-span-2' : '',
-      'relative pb-1/1 rounded-lg shadow',
-      'bg-white dark:bg-card',
-    )}
-  >
-    <a href={edge.permalink} rel="noopener noreferrer" target="_blank">
-      <Image
-        className="rounded-lg"
-        src={edge.thumbnail_url || edge.media_url}
-        alt={edge.caption}
-        placeholder="blur"
-        blurDataURL={edge.thumbnail_url || edge.media_url}
-        layout="fill"
-        objectFit="cover"
-      />
-    </a>
-  </div>
-);
+const InstagramItem: React.FC<Props> = ({ index, edge }) => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  return (
+    <div
+      key={edge.id}
+      className={clsx(
+        index === 0 ? 'lg:col-span-2 lg:row-span-2' : '',
+        !isLoaded
+          ? 'bg-gray-200 dark:bg-gray-700 rounded-lg shadow motion-safe:animate-pulse'
+          : '',
+        'relative pb-1/1 ',
+      )}
+    >
+      <a href={edge.permalink} rel="noopener noreferrer" target="_blank">
+        <Image
+          className="rounded-lg"
+          src={edge.thumbnail_url || edge.media_url}
+          alt={edge.caption}
+          placeholder="blur"
+          blurDataURL={edge.thumbnail_url || edge.media_url}
+          layout="fill"
+          objectFit="cover"
+          onLoadingComplete={() => setIsLoaded(true)}
+        />
+      </a>
+    </div>
+  );
+};
 
 export default InstagramItem;
--- src/components/last-fm/LastFmItem.tsx
+++ src/components/last-fm/LastFmItem.tsx
@@ -1,4 +1,7 @@
+import { useState } from 'react';
+
 import { PlayIcon } from '@heroicons/react/solid';
+import clsx from 'clsx';
 import Image from 'next/image';
 
 import { LastFmTrack } from '@/types/entities';
@@ -7,46 +10,58 @@ interface Props {
   track: LastFmTrack;
 }
 
-const LastFmItem: React.FC<Props> = ({ track }) => (
-  <div className="flex flex-col p-2 h-full bg-white dark:bg-card rounded-lg shadow min-h-36">
-    <a
-      href="https://last.fm/user/RajceP"
-      rel="noopener noreferrer"
-      target="_blank"
-    >
-      <div className="flex flex-row space-x-2">
-        <div className="w-32 h-32 rounded-lg shadow">
-          <Image
-            className="rounded-lg"
-            src={track.image[2]['#text']}
-            alt="Album art"
-            layout="fixed"
-            width={128}
-            height={128}
-            placeholder="blur"
-            blurDataURL={track.image[0]['#text']}
-          />
+const LastFmItem: React.FC<Props> = ({ track }) => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  return (
+    <div className="flex flex-col p-2 h-full bg-white dark:bg-card rounded-lg shadow min-h-36">
+      <a
+        href="https://last.fm/user/RajceP"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        <div className="flex flex-row space-x-2">
+          <div
+            className={clsx(
+              !isLoaded
+                ? 'bg-gray-200 dark:bg-gray-700 rounded-lg shadow motion-safe:animate-pulse'
+                : '',
+              'w-32 h-32 rounded-lg shadow',
+            )}
+          >
+            <Image
+              className="rounded-lg"
+              src={track.image[2]['#text']}
+              alt="Album art"
+              layout="fixed"
+              width={128}
+              height={128}
+              placeholder="blur"
+              blurDataURL={track.image[0]['#text']}
+              onLoadingComplete={() => setIsLoaded(true)}
+            />
+          </div>
+          <div className="flex flex-col min-h-full">
+            <h3 className="text-lg font-semibold dark:text-gray-100">
+              {track.name}
+            </h3>
+            <div className="flex flex-1"></div>
+            <p className="text-gray-500 dark:text-gray-400">
+              {track.artist['#text']}
+            </p>
+            <p className="text-gray-500 dark:text-gray-400">
+              {track.album['#text']}
+            </p>
+          </div>
+          {track['@attr']?.nowplaying && (
+            <span className="flex grow justify-end w-12 h-12 text-red-600 motion-safe:animate-pulse items-top">
+              <PlayIcon />
+            </span>
+          )}
         </div>
-        <div className="flex flex-col min-h-full">
-          <h3 className="text-lg font-semibold dark:text-gray-100">
-            {track.name}
-          </h3>
-          <div className="flex flex-1"></div>
-          <p className="text-gray-500 dark:text-gray-400">
-            {track.artist['#text']}
-          </p>
-          <p className="text-gray-500 dark:text-gray-400">
-            {track.album['#text']}
-          </p>
-        </div>
-        {track['@attr']?.nowplaying && (
-          <span className="flex grow justify-end w-12 h-12 text-red-600 motion-safe:animate-pulse items-top">
-            <PlayIcon />
-          </span>
-        )}
-      </div>
-    </a>
-  </div>
-);
+      </a>
+    </div>
+  );
+};
 
 export default LastFmItem;

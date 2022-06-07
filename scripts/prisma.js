15e6041c109141cefcefe18630abf9c7a6cd66d3
Petr Rajtslegr <petr.rajtslegr@gmail.com>
2022-06-07 14:54:46 +0200

    :rotating_light: Lint scripts files

--- scripts/prisma.js
+++ scripts/prisma.js
@@ -3,23 +3,23 @@ process.chdir(`${__dirname}/..`);
 
 const args = process.argv.slice(2);
 const nodeEnv = args.shift();
-const envs = { dev: "development", prod: "production" };
+const envs = { dev: 'development', prod: 'production' };
 
 if (!Object.keys(envs).includes(nodeEnv)) {
-  process.stderr.write("Usage: prisma [dev|prod] [prisma-commands]\n");
+  process.stderr.write('Usage: prisma [dev|prod] [prisma-commands]\n');
   process.exit(0);
 }
 
 process.env.NODE_ENV = envs[nodeEnv];
 
-require("dotenv-flow").config();
+require('dotenv-flow').config();
 
-const { spawn } = require("child_process");
+const { spawn } = require('child_process');
 
-const opts = { stdio: "inherit" };
+const opts = { stdio: 'inherit' };
 
-if (process.platform === "win32") {
-  spawn("cmd", ["/c", "node_modules\\.bin\\prisma.cmd", ...args], opts);
+if (process.platform === 'win32') {
+  spawn('cmd', ['/c', 'node_modules\\.bin\\prisma.cmd', ...args], opts);
 } else {
-  spawn("node_modules/.bin/prisma", args, opts);
+  spawn('node_modules/.bin/prisma', args, opts);
 }

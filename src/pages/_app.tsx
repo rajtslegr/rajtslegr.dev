562daa866719e440a003bcd7714fbebb25ec38ba
Petr Rajtslegr <petr.rajtslegr@gmail.com>
2022-12-10 20:03:09 +0100

    :chart_with_upwards_trend: Add Verce Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { Inter } from '@next/font/google';
+import { Analytics } from '@vercel/analytics/react';
 import { AnimatePresence } from 'framer-motion';
 import { ThemeProvider } from 'next-themes';
 import { AppProps } from 'next/app';
@@ -6,25 +7,21 @@ import '@/styles/globals.css';
 import NextNProgress from 'nextjs-progressbar';
 
 import Layout from '@/components/layout/Layout';
-import usePanelbear from '@/hooks/usePanelbear';
 
 const interVariable = Inter({ subsets: ['latin'] });
 
-const App: React.FC<AppProps> = ({ Component, pageProps }) => {
-  usePanelbear('8nvtaxIaLJM');
-
-  return (
-    <ThemeProvider defaultTheme="system" attribute="class">
-      <Layout>
-        <NextNProgress options={{ showSpinner: false }} />
-        <AnimatePresence mode="wait">
-          <main className={interVariable.className}>
-            <Component {...pageProps} />
-          </main>
-        </AnimatePresence>
-      </Layout>
-    </ThemeProvider>
-  );
-};
+const App: React.FC<AppProps> = ({ Component, pageProps }) => (
+  <ThemeProvider defaultTheme="system" attribute="class">
+    <Layout>
+      <NextNProgress options={{ showSpinner: false }} />
+      <AnimatePresence mode="wait">
+        <main className={interVariable.className}>
+          <Component {...pageProps} />
+          <Analytics />
+        </main>
+      </AnimatePresence>
+    </Layout>
+  </ThemeProvider>
+);
 
 export default App;
